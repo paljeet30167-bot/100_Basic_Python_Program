73dc484542e0864766480e3b3c1a59d414199dde
Jitendra <paljeet30167@gmail.com>
2025-10-30 14:59:33 +0530

    Added program to add two numbers
    This program takes two numbers and print their sum.

---
+++ Program1.py
@@ -0,0 +1,18 @@
+ # Write a Program to find the sum of two variables? With Predefined Variables.                                  
+# Author : Jitendra
+# Date : 30-10-2025
+
+num1 = 30
+num2 = 40
+num3 = num1+num2
+print("The sum of num1 and num2 is :", num3)
+
+
+
+
+# Through the user Input
+
+num1 = float(input("Enter the Number1: "))
+num2 = float(input("Enter the Number2: "))
+print("The sum of Number1 and Number2 is:",num1+num2)
+
