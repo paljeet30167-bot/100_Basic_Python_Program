55c4d843f0b9069c7e7e6eacd7ed67b55a29ef23
Jitendra <paljeet30167@gmail.com>
2025-10-31 12:43:47 +0530

    Program to find the Square root.

---
+++ Program2.py
@@ -0,0 +1,16 @@
+# Write a Python program to find the square root. With the Exponent Operator
+
+num = 64
+num1 = int(input("Enter a number here:"))
+sr = num1**(1/2)
+print("Square root of num is : ", sr)
+
+
+#Solution 2 -( With the math Modules)
+import math
+num = int(input("Enter the a number here:"))
+sr = math.sqrt(num)
+print("Square root of the given is :",sr)
+
+
+
