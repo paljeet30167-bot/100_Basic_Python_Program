993b93bdfa01483ef8df757131ff4a0a5bc5cd2e
Jitendra <paljeet30167@gmail.com>
2025-11-04 11:27:01 +0530

    Python Program to calculate the Area of a Tringle
    # Write a Python Program to calculate the Area of a Tringle

---
+++ Problem3.py
@@ -0,0 +1,13 @@
+# Write a Python Program to calculate the Area of a Tringle
+
+base = 32
+height = 33
+area = 1/2*32*33
+print("Area of Tringle is:",area)
+
+
+#solution (with user input)
+num1 = int(input("Enter the base:"))
+num2 = int(input("Enter the height:"))
+area = 1/2*num*num1
+print("print the area of the tringle is ;" ,area)
